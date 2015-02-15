4eeee84b5d1fd96c69e559300fdac5f497242be3
dg <dg@isembard.(none)>
2015-02-15 12:42:58 +0000

    working

--- stepping/stepping.ino
+++ stepping/stepping.ino
@@ -6,7 +6,6 @@ using namespace std;
 #define signed_int_16 short int
 
 
-unsigned char orientation_based_current = 6;
 
 // ########################Hardware defs################################
 
@@ -16,16 +15,12 @@ unsigned char orientation_based_current = 6;
 #define STEPPER_1_DDR  DDRD
 
 
-#define STEPPER_1_PWM_MASK 0B00000100
-#define STEPPER_1_DIR_MASK 0B01000000
+#define STEPPER_1_PWM_MASK 0B00001000
+#define STEPPER_1_DIR_MASK 0B00000100
 
-#define MS1_MASK 0B00010000
-#define MS2_MASK 0B00100000
-#define MS3_MASK 0B00100000
-
-#define LIMIT_0 12
-#define LIMIT_1 13
-#define IN_PIN  A0
+#define LIMIT_0 4
+#define LIMIT_1 5
+#define IN_PIN  A3
 
 #define SET(REGISTER, VALUE) REGISTER |=  (VALUE)
 #define CLR(REGISTER, VALUE) REGISTER &= ~(VALUE)
@@ -40,10 +35,6 @@ typedef long int s32int;
 
 const u32int dither_frequency  = 30000;  //hz
 
-const unsigned char microstepping_settings[4] = {
-  0, MS1_MASK, MS2_MASK, MS1_MASK | MS2_MASK}; // TODO - add 16th step?
-
-const unsigned char stepper_1_ms_level = 3;
 const double mm_per_step = 0.005; // TODO Calibrate
 
 
@@ -51,8 +42,8 @@ int step_ticker_1 = 0;
 
 long int step_count_1 = 0;
 
-const long int max_acceleration = 2800; //  (steps per seccond per second)
-const int max_step_rate = 1200;
+const long int max_acceleration = 10000; //  (steps per seccond per second)
+const int max_step_rate = 10000;
 
 const int stepper_timer_period = 1e6/dither_frequency;
 const u32int counter_overflow = 0xffffffff;
@@ -64,7 +55,7 @@ const s32int a = (counter_overflow / dither_frequency);
 const s32int b = (s32int)(a) * max_acceleration;
 
 
-const u32int max_increment_jump_per_dither_step = b / dither_frequency;
+const u32int max_increment_jump_per_dither_step = b / dither_frequency*10;
 
 double rate_1_target = 0;
 
@@ -82,11 +73,12 @@ int position_updated = 0;
 // #################################### Steppiing subs #########################################
 void set_rate(double step_rate, long int & increment)
 {
-  if      (step_rate >  max_step_rate*10) step_rate =  max_step_rate*10;
-  else if (step_rate < -max_step_rate*10) step_rate = -max_step_rate*10;
+  if      (step_rate >  max_step_rate) step_rate =  max_step_rate;
+  else if (step_rate < -max_step_rate) step_rate = -max_step_rate;
 
-  // s32int step_rate_times_10 = (s32int) (step_rate * 10.0);
-  increment = multiplier * (s32int) step_rate;
+  s32int step_rate_times_10 = (s32int) (step_rate * 10.0);
+
+  increment = multiplier * (s32int) step_rate_times_10;
 }
 
 void set_rate_1(double step_rate)
@@ -116,7 +108,7 @@ void timer_one_isr()
   DO_ACCELERATION(stepper_increment_1, stepper_increment_1_target);
 
   bool test_1 = (counter_1 > 0);
-  counter_1 += stepper_increment_1 << stepper_1_ms_level;
+  counter_1 += stepper_increment_1;
   bool test_2 = (counter_1 > 0);
 
   if (test_2 & !test_1)
@@ -124,13 +116,13 @@ void timer_one_isr()
 
     if ( stepper_increment_1 < 0)
     {
-      STEPPER_1_PORT &= ~STEPPER_1_DIR_MASK;
+      STEPPER_1_PORT |= STEPPER_1_DIR_MASK;
       step_ticker_1--;
     }
     else
     {
 
-      STEPPER_1_PORT |= STEPPER_1_DIR_MASK;
+      STEPPER_1_PORT &= ~STEPPER_1_DIR_MASK;
       step_ticker_1++;
     }
 
@@ -159,34 +151,71 @@ void toggle_light()
 enum t_mode
 {
   HUNTING,
-  TRACKING
+  TRACKING,
+  STOP
 };
 
 t_mode mode;
+
+void set_microsteps(int level)
+{
+  switch(level)
+  {
+    case (0):
+      digitalWrite(8,0);
+      digitalWrite(7,0);
+      digitalWrite(6,0);
+      break;
+    case (1):
+      digitalWrite(8,1);
+      digitalWrite(7,0);
+      digitalWrite(6,0);
+      break;
+    case (2):
+      digitalWrite(8,0);
+      digitalWrite(7,1);
+      digitalWrite(6,0);
+      break;
+    case (3):
+      digitalWrite(8,1);
+      digitalWrite(7,1);
+      digitalWrite(6,0);
+      break;
+    case (4):
+      digitalWrite(8,0);
+      digitalWrite(7,0);
+      digitalWrite(6,1);
+      break;
+    case (5):
+      digitalWrite(8,1);
+      digitalWrite(7,0);
+      digitalWrite(6,1);
+      break;
+  }
+}
+
+void do_output(double value)
+{
+  analogWrite(OUTPUT_PIN, PWM_FROM_VOLTAGE(value));
+}
 
 void setup() {
 
   // Configure outputs
 
   CLR(STEPPER_1_PORT, STEPPER_1_PWM_MASK | STEPPER_1_DIR_MASK);
-  CLR(STEPPER_1_PORT, MS1_MASK | MS2_MASK);
+
+  SET(STEPPER_1_DDR,  STEPPER_1_PWM_MASK | STEPPER_1_DIR_MASK );
+
+  set_microsteps(2);
 
 
-  SET(STEPPER_1_DDR,  STEPPER_1_PWM_MASK | STEPPER_1_DIR_MASK | MS1_MASK | MS2_MASK);
-
-  //to do SETUP MICROSTEPPING
-
-
-  SET(STEPPER_1_PORT, microstepping_settings[stepper_1_ms_level]);
-
-
-  pinMode(OUTPUT_PIN, OUTPUT);
+  do_output(0);
   
   TCCR2B = (TCCR2B & 0b11111000) | 0x1; // Set PWM clock divider for current limit pwm to 1.
 
   Timer1.initialize(stepper_timer_period);
   Timer1.attachInterrupt( timer_one_isr );
-  analogWrite(OUTPUT_PIN, 0);
 
 
 // TODO Flash state light?
@@ -198,9 +227,13 @@ void setup() {
 
   delay(1000);
 
-  set_rate_1 (-max_speed);
+
+set_rate_1(-max_speed);//
+
+
   while (!digitalRead(LIMIT_0))
     {}
+
   set_rate_1(0);
   step_count_1 = -1000; // Reset zero
   
@@ -214,6 +247,12 @@ void setup() {
 void hunt ()
 {
   static int dir = 1;
+  if (digitalRead(LIMIT_1) && digitalRead(LIMIT_0))
+  {
+     mode = STOP;
+     return;
+  }
+
   if (digitalRead(LIMIT_0)) dir = 1;
   if (digitalRead(LIMIT_1)) dir = -1;
 
@@ -237,6 +276,20 @@ void hunt ()
 
 void track()
 {
+
+  if (digitalRead(LIMIT_1) && digitalRead(LIMIT_0))
+  {
+     mode = STOP;
+     return;
+  }
+  if (digitalRead(LIMIT_1) || digitalRead(LIMIT_0))
+  {
+     mode = HUNTING;
+     return;
+  }
+
+
+
   static int dir = -10;
   if (new_track) 
    {
@@ -274,6 +327,8 @@ void loop() {
        track();
        // TODO if (position_updated) do something
        break;
+     default:
+       set_rate_1(0);
   }
   
   /// TOOD check for timeout
